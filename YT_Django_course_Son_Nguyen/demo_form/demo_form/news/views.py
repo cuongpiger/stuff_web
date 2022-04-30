7539f3db1c6d0d65e3c5ee310e5950309950a00d
Manh Cuong <cuongpigerr@gmail.com>
2022-04-30 22:07:47 +0700

    done lesson 11: create a form

--- YT_Django_course_Son_Nguyen/demo_form/demo_form/news/views.py
+++ YT_Django_course_Son_Nguyen/demo_form/demo_form/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import PostForm
+from .forms import PostForm, SendEmail
 
 def index(request):
     return HttpResponse("Hello World!")
@@ -23,3 +23,31 @@ def save_news(request):
     else:
         return HttpResponse("This is not a POST request!")
     
+    
+def email_view(request):
+    b = SendEmail()
+    return render(request, 'news/email.html', {'f': b})
+
+
+def process(request):
+    if request.method == 'POST':
+        m = SendEmail(request.POST)
+        
+        if m.is_valid():
+            tieude = m.cleaned_data.get('title')
+            cc = m.cleaned_data.get('cc')
+            noidung = m.cleaned_data.get('content')
+            email = m.cleaned_data.get('email')
+            
+            context = {'td': tieude, 'c': cc, 
+                       'b': noidung, 'd': email}
+            
+            context2 = {'email_data': m}
+            
+            # return render(request, 'news/print_email.html', context)
+            return render(request, 'news/print_email.html', context2)
+        
+        else:
+            return HttpResponse("Form not validate!")
+    else:
+        return HttpResponse("This is not a POST method!")
