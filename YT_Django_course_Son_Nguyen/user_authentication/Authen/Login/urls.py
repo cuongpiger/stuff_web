06b79484f67cb2690e1519b2ec9a696966ceb721
Manh Cuong <cuongpigerr@gmail.com>
2022-05-01 20:31:17 +0700

    done lesson 17: user login and its hierarchy

--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
@@ -4,5 +4,5 @@ from .views import IndexClass, LoginClass, ViewUser
 urlpatterns = [
     path('', IndexClass.as_view(), name='index'),
     path('login/', LoginClass.as_view(), name='login'),
-    path('user-view/', ViewUser.as_view(), name='user_view'),
+    path('user-view/', ViewUser.as_view(), name='user_view')
 ]
