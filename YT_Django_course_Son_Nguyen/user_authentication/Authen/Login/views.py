5f2fdc6b756d0ca09c0ddf689ae7783833ba5b8b
Manh Cuong <cuongpigerr@gmail.com>
2022-05-01 19:43:16 +0700

    done lesson 16: user model

---
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from django.views import View
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+class IndexClass(View):
+    def get(self, request):
+        return HttpResponse("<h1>Hello</h1>")
+
+
