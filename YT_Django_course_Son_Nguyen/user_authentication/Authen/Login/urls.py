d0b2fe3626c24a25863e5a17145c16ccfd7b5788
Manh Cuong <cuongpigerr@gmail.com>
2022-05-01 20:18:57 +0700

    done lesson 15: login user with authentication

--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexClass
+from .views import IndexClass, LoginClass
 
 urlpatterns = [
     path('', IndexClass.as_view(), name='index'),
+    path('login/', LoginClass.as_view(), name='login')
 ]
--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 # Create your views here.
 
 
@@ -9,3 +9,15 @@ class IndexClass(View):
         return HttpResponse("<h1>Hello</h1>")
 
 
+class LoginClass(View):
+    def get(self, request):
+        return render(request, 'Login/login.html')
+    
+    def post(self, request):
+        user_name = request.POST.get('tendangnhap') # get username from file login.html
+        password = request.POST.get('password') # get password from file login.html
+        my_user = authenticate(username=user_name, password=password) # check username and password
+        
+        if my_user is None:
+            return HttpResponse("This user does not exist")
+        return HttpResponse(f"You have logged in with {user_name} - {password}")
