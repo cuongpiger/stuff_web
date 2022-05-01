bd67645e8cd77927cb1940ed2705f3562f327b5d
Manh Cuong <cuongpigerr@gmail.com>
2022-05-01 21:31:09 +0700

    done lesson 18: decoratora and LoginRequiredMixin

--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexClass, LoginClass, ViewUser
+from .views import IndexClass, LoginClass, ViewUser, view_product
 
 urlpatterns = [
     path('', IndexClass.as_view(), name='index'),
     path('login/', LoginClass.as_view(), name='login'),
-    path('user-view/', ViewUser.as_view(), name='user_view')
+    path('user-view/', ViewUser.as_view(), name='user_view'),
+    path('view-pro/', view_product, name='view-product'),
 ]
--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, decorators
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -27,9 +28,17 @@ class LoginClass(View):
         return render(request, 'Login/thanhcong.html')
     
     
-class ViewUser(View):
+class ViewUser(LoginRequiredMixin, View): # only user who is logged in can access these views in this class
+    login_url = '/login/' # user will be redirected to this url if he/she is not logged in
     def get(self, request):
-        if not request.user.is_authenticated:
-            return HttpResponse("You are not login")
-        else:
             return HttpResponse("<h1>This is user view</h1>")
+        
+    def post(self, request):
+        pass
+        
+        
+# if user go to this url without login, it will redirect to login page
+@decorators.login_required(login_url='/login/')
+def view_product(request):
+    # only valid user can access this url
+    return HttpResponse("View product")
