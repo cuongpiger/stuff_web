de8ca0579fdbdc6db7149944980cf3a7db5d54ba
Manh Cuong <cuongpigerr@gmail.com>
2022-05-01 22:59:34 +0700

    done lesson 20: set role of user

--- YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
+++ YT_Django_course_Son_Nguyen/user_authentication/Authen/Login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from django.views import View
 from django.contrib.auth import authenticate, login, decorators
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostForm
 # Create your views here.
 
 
@@ -42,3 +43,30 @@ class ViewUser(LoginRequiredMixin, View): # only user who is logged in can acces
 def view_product(request):
     # only valid user can access this url
     return HttpResponse("View product")
+
+
+class AddPost(LoginRequiredMixin, View):
+    login_url = '/login/' # user will be redirected to this url if he/she is not logged in
+    def get(self, request):
+        f = PostForm()
+        context = {
+            'fm': f
+        }
+        
+        return render(request, 'Login/add_post.html', context)
+    
+    
+    def post(self, request):
+        f = PostForm(request.POST)
+        
+        if not f.is_valid():
+            return HttpResponse("Invalid form")
+        
+        print(request.user.get_all_permissions())
+        if request.user.has_perm('Login.add_post'): # model name have to be lower-case
+            # user has permission to add post
+            # save data into database
+            f.save()
+        else:
+            return HttpResponse("You do not have permission to add post")
+        return HttpResponse('OK')
