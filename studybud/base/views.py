f3b54a20fa9682f65d53ff7f90de415ab060b210
Manh Cuong <cuongpigerr@gmail.com>
2022-05-04 13:35:07 +0700

    delete messages from room

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -84,8 +84,22 @@ def home(request):
 
 # URL has parameters
 def room(request, pk):
-    room = Room.objects.get(id=pk) # get room by id
-    context = {'room': room}
+    room:Room = Room.objects.get(id=pk) # get room by id
+    room_messages = room.message_set.all().order_by('-created') # get all messages from room with order by created
+    participants = room.participants.all()
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+    
+        room.participants.add(request.user)
+        
+        return redirect('room', pk=room.id) # redirect to same page
+    
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -139,3 +153,18 @@ def deleteRoom(request, pk):
         return redirect('home')
     
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    # if user try to edit the room that is not his own
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete this room!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj': message})
