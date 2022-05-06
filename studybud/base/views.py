8f8f6b8556bad6ffefc2804584cb2adecd0a95d4
Manh Cuong <cuongpigerr@gmail.com>
2022-05-06 23:17:17 +0700

    mobile responsiveness

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -75,7 +75,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)) # get value based on foreign key (video at 2:14:26)
     
-    topics = Topic.objects.all() # get all topics
+    topics = Topic.objects.all()[:5] # get first 5 topics
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
@@ -222,3 +222,17 @@ def updateUser(request):
     return render(request, 'base/update_user.html', {
         'form': form,
     })
+    
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else '' # get value from parameters of URL
+    topics = Topic.objects.filter(name__icontains=q) # get all topics with name containing q
+    
+    return render(request, 'base/topics.html', {
+        'topics': topics,
+    })
+    
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
