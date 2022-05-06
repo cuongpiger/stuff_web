64d9e3ffcc55403f8176408a011d850c4a61c544
Manh Cuong <cuongpigerr@gmail.com>
2022-05-06 22:09:16 +0700

    edit user account page

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 
 class RoomForm(ModelForm):
@@ -7,3 +8,10 @@ class RoomForm(ModelForm):
         model = Room
         fields = '__all__' # auto generate form components which match to all fields in the model 
         exclude = ['host', 'participants'] # exclude some fields
+        
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        # fields = '__all__'
+        fields = ['username', 'email']
