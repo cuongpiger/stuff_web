675bc3e03b1137b27dfd6bacff7245b6a4cc3e51
Manh Cuong <cuongpigerr@gmail.com>
2022-05-14 14:37:32 +0700

    12 url config and dispatcher

--- dh_mo_django/courses/urls.py
+++ dh_mo_django/courses/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from .admin import admin_site
 
 urlpatterns = [
     path('', views.index, name='index'),
     path("admin/", admin_site.urls),
+    path('welcome/<int:year>/', views.welcome, name='welcome'),
+    re_path(r'^welcome2/(?P<year>[0-9]{1,2})/$', views.welcome2, name='welcome2'), # take from 1 to 2 digits, lấy đúng 4 kí tự thì thay {1,2} thành {4}
+    path('test/', views.TestView.as_view(), name='test'),
 ] 
--- dh_mo_django/courses/views.py
+++ dh_mo_django/courses/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 def index(request):
     return render(request, "index.html", context={
-        'name': "Thanh Duong"
+        'name': "Manh Cuong"
     })
+    
+    
+
+def welcome(request, year:int):
+    return HttpResponse(f"HELLO - {year}")
+
+def welcome2(request, year:int):
+    return HttpResponse(f"HELLO-2 - {year}")
+
+
+class TestView(View):
+    def get(self, request):
+        return HttpResponse("Welcome testing")
+    
+    def post(self, request):
+        pass
